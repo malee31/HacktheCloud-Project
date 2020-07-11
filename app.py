de6e49196d9548f541a6a923f576e5163d6b51cb
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-07-11 10:53:33 -0700

    Whoops, rerouted

--- app.py
+++ app.py
@@ -11,17 +11,7 @@ app.config.update(
 @app.route("/", methods = ["GET"])
 def home():
 	print(__name__)
-	return render_template("home.html", id=2)
-
-@app.route("/todo/<id>", methods = ["GET"])
-def todo(id):
-	return render_template("list.html", listId = id)
-
-@app.errorhandler(404)
-def notFound(e):
-	print("File not found", e)
-	return redirect(url_for("home"))
-
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run(debug = True)
