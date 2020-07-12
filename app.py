3f1e5b484af94c47a2cf26c600916b9b6a500861
ling1729 <renlingfeng21@gmail.com>
2020-07-12 11:47:11 -0700

    dynamodb??

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import os
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, jsonify
+
 app = Flask(__name__, template_folder = "./src/templates", static_folder = "./src/static")
 
 app.config.update(
-	TESTING = True,
-	SECRET_KEY = os.urandom(32)
+        TESTING = True,
+        SECRET_KEY = os.urandom(32)
 )
 
 @app.route("/home", methods = ["GET"])
 @app.route("/", methods = ["GET"])
+@app.route('/get-items')
+
+def get_items():
+    return jsonify(aws_controller.get_items())
+
 def home():
-	print(__name__)
-	return render_template("index.html")
+        print(__name__)
+        return render_template("index.html")
 
 @app.route("/welcome", methods = ["GET"])
 def display():
-	return render_template("welcome.html");
+        return render_template("welcome.html");
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="80")
+        app.run(host="0.0.0.0", port="80")
