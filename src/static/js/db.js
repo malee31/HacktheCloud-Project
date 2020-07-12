80788d9fe5a0f0eef16e082313d51fb9f1c59b08
ling1729 <renlingfeng21@gmail.com>
2020-07-12 14:03:49 -0700

    change name

--- src/static/js/db.js
+++ src/static/js/db.js
@@ -2,15 +2,24 @@ function getMessages()
 {
 	let xhr = new XMLHttpRequest();
 	xhr.addEventListener("load", data => {
-		console.log(data);
+		var resJSON = JSON.parse(data.currentTarget.responseText);
+		return resJSON;
+	});
+	xhr.open("GET", window.location.origin + "/read");
+	xhr.send();
+}
+
+function showMessages(id){
+	document.getElementById(id).innerHTML = getMessages();
+}
+
+function sendMessage(message, user){
+	let xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", data => {
 		var resJSON = JSON.parse(data.currentTarget.responseText);
 		return resJSON;
 	});
 	xhr.open("GET", window.location.origin + "/database");
-	xhr.send();
-}
-function showMessages(id){
-	document.getElementById(id).innerHTML = getMessages();
-}
-function sendMessage(messag, user){
+	xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xhr.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }));
 }
