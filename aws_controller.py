d0ef32147a75af023b89bee6bfa57b0bc84ad894
ling1729 <renlingfeng21@gmail.com>
2020-07-12 13:26:58 -0700

    convert everything to tabs

--- aws_controller.py
+++ aws_controller.py
@@ -3,6 +3,6 @@ import boto3
 dynamo_client = boto3.client('dynamodb')
 
 def getItems():
-	return dynamo_client.scan(
-		TableName='Messages'
-	)
+		return dynamo_client.scan(
+				TableName='Messages'
+		)
